ccf95c944c7c6cc82afda6d0e26fc59be5425e2f
Jason Quense <monastic.panic@gmail.com>
2018-10-18 11:12:38 -0400

    feat: support FormattedMessage intl components for namespaces

--- namespace-plugin.js
+++ namespace-plugin.js
@@ -1,19 +1,46 @@
 const DEFINE_MESSAGES = 'defineMessages';
 
+const COMPONENT_NAMES = ['FormattedMessage', 'FormattedHTMLMessage'];
+
 module.exports = function namespacePlugin({ types: t }) {
+  function getPrefix(state) {
+    let PREFIX = state.opts.prefix;
+    if (!PREFIX.endsWith(':')) PREFIX = `${PREFIX}:`;
+  }
+  function referencesImport(path) {
+    if (!(path.isIdentifier() || path.isJSXIdentifier())) return false;
+    return COMPONENT_NAMES.some(name =>
+      path.referencesImport('react-intl', name),
+    );
+  }
+
   return {
     visitor: {
+      JSXOpeningElement(path, state) {
+        const name = path.get('name');
+        if (!referencesImport(name)) return;
+
+        const PREFIX = getPrefix(state);
+
+        const idAttr = path
+          .get('attributes')
+          .find(attr => attr.isJSXAttribute() && attr.node.name.name === 'id');
+
+        if (idAttr && !idAttr.node.value.value.startsWith(PREFIX)) {
+          idAttr
+            .get('value')
+            .replaceWith(t.StringLiteral(PREFIX + idAttr.node.value.value));
+        }
+      },
+
       CallExpression(path, state) {
-        let PREFIX = state.opts.prefix;
         const callee = path.get('callee').node;
 
         if (!t.isIdentifier(callee) || callee.name !== DEFINE_MESSAGES) {
           return;
         }
 
-        if (!PREFIX.endsWith(':')) {
-          PREFIX = `${PREFIX}:`;
-        }
+        const PREFIX = getPrefix(state);
 
         function processMessageObject(messageObj) {
           if (!messageObj || !messageObj.isObjectExpression()) {
